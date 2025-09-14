c818d3542ab7793db9defeb1ab6b277e6ab26c53
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-09-14 16:09:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let mybutton=document.getElementById("incrementBtn");
+let myPara=document.getElementById("counter");
+
+let c=0;
+mybutton.addEventListener("click",()=>{
+	myPara.textContent=c+1;
+	alert(c);
+	c++;
+})
